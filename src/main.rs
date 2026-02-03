0302a2b6823e08be5590f54ce0e42e5fd42c204e
kevinkurek <kevinkurek@live.com>
2026-02-03 11:33:28 -0800

    server up and base endpoints working

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::net::SocketAddr;
+use axum::{
+    routing::{delete, get, post},
+    Router};
+mod handlers;
+mod models;
+use handlers::*;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/question", post(create_question))
+        .route("/questions", get(read_questions))
+        .route("/question", delete(delete_question))
+        .route("/answer", post(create_answer))
+        .route("/answers", get(read_answers))
+        .route("/answer", delete(delete_answer));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+// automatically restart server when source files change
+// cargo watch -q -c -w src/ -x run
+
+// sample CURL POST
+// curl -X POST http://localhost:8000/question \
+//   -H 'Content-Type: application/json' \
+//   -d '{"title":"Title","description":"Description"}'
